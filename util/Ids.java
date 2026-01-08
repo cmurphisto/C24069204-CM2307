e3042184c42f97a4ad5c0b996d2caf7e79c712ba
cmurphisto <murphyc23@cardiff.ac.uk>
2026-01-08 03:15:19 +0000

    Ids v1.1
    Added UUID import and newId method for ID generation.

--- util/Ids.java
+++ util/Ids.java
@@ -1,7 +1,9 @@
 package studentrentals.util; 
-import java.util.*; 
+
+import java.util.UUID; 
 
 public class Ids {
+    private Ids() {}   //Prevent instantiation
     public static String newId() {
         return UUID.randomUUID().toString();
     }
