1700c2ef443f53e694d34ce67db4a16f6ce06f55
cmurphisto <murphyc23@cardiff.ac.uk>
2026-01-08 08:16:20 +0000

    property repository final

--- repo/PropertyRepository.java
+++ repo/PropertyRepository.java
@@ -1,5 +1,15 @@
-package studentrentals.repo; 
+package studentrentals.repo;
 
-public class PropertyRepository {
-    
+import studentrentals.model.property.Property;
+
+import java.util.*;
+
+public final class PropertyRepository {
+    private final Map<String, Property> byId = new HashMap<>();
+
+    public void save(Property property) { byId.put(property.id(), property); }
+    public Optional<Property> findById(String id) { return Optional.ofNullable(byId.get(id)); }
+    public Collection<Property> findAll() { return Collections.unmodifiableCollection(byId.values()); }
+
+    public void delete(String propertyId) { byId.remove(propertyId); }
 }
