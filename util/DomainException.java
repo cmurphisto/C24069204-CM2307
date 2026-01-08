b0e4338d7fffa5b64bd2054cc8632c0733e4a1d0
cmurphisto <murphyc23@cardiff.ac.uk>
2026-01-08 08:08:29 +0000

    Domain Exception FINAL

--- util/DomainException.java
+++ util/DomainException.java
@@ -1,7 +1,6 @@
 package studentrentals.util;
 
 public class DomainException extends RuntimeException {
-    //Print message for the exception
     public DomainException(String message) {
         super(message);
     }
